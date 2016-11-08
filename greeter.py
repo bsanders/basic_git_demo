94aac511ee9ce4457d5d116ec23053b7a09d7788
bill <billysanders@gmail.com>
2016-11-08 20:51:04 +0000

    bug

--- greeter.py
+++ greeter.py
@@ -3,4 +3,9 @@ from __future__ import print_function
 
 import sys
 
-print('hello {0}'.format(sys.argv[1]))
+if len(sys.argv) > 1:
+  name = sys.argv[1]
+  print('hello {0}'.format(name.title()))
+else:
+  print('you did it wrong')
+  sys.exit(1)
